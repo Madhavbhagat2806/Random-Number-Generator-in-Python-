e779c6ca7ef5ea0662007d92305dbf7e9059d22f
Madhavbhagat2806 <110768692+Madhavbhagat2806@users.noreply.github.com>
2024-07-02 22:48:10 +0530

    Add files via upload

---
+++ RANDOM.PY.py
@@ -0,0 +1,13 @@
+# Random   password Generator
+import random
+import string
+
+
+pass_len=12
+char_values= string.ascii_letters + string.digits + string.punctuation
+
+password =  ""
+for i in range(pass_len):
+    password +=random.choice(char_values)
+
+print("Your password is:", password)
